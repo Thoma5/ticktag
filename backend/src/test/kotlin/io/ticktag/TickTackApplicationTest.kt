cf1025418ca9f574d3c9e894d8d024d80c17e24d
Michael Kainer <michael.kainer@student.tuwien.ac.at>
2016-11-01 16:25:24 +0100

    Fix initialization test

--- backend/src/test/kotlin/io/ticktag/TickTackApplicationTest.kt
+++ backend/src/test/kotlin/io/ticktag/TickTackApplicationTest.kt
@@ -1,14 +1,12 @@
 package io.ticktag
 
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
-import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext
 
-@RunWith(JUnit4::class)
 class TickTackApplicationTest {
     @Test
     fun context_isInitializedSuccessfully() {
-        val context = AnnotationConfigApplicationContext(TicktagApplication::class.java)
+        val context = AnnotationConfigWebApplicationContext()
+        context.register(TicktagApplication::class.java)
     }
 }
