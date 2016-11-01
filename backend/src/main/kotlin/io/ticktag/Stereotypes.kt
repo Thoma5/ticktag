e069354d4af7151fb6fbd2bfeb393b7f54a8d575
Simon Strassl <stuff@sigmoid.at>
2016-11-01 14:39:13 +0100

    Add spring-tx

--- backend/src/main/kotlin/io/ticktag/Stereotypes.kt
+++ backend/src/main/kotlin/io/ticktag/Stereotypes.kt
@@ -2,6 +2,7 @@ package io.ticktag
 
 import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.RestController
 
 @Repository
@@ -10,8 +11,9 @@ annotation class TicktagRepository
 @RestController
 annotation class TicktagRestInterface
 
-// TODO: Add transactions and default security
+// TODO: Add default security
 @Service
+@Transactional
 annotation class TicktagService
 
 @Service
