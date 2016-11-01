ec2df64da59dbf2e645374c08353eee58bf47c31
Simon Strassl <stuff@sigmoid.at>
2016-11-01 16:27:13 +0100

    Remove rest controller interface

--- backend/src/main/kotlin/io/ticktag/restinterface/hello/controllers/HelloController.kt
+++ backend/src/main/kotlin/io/ticktag/restinterface/hello/controllers/HelloController.kt
@@ -1,8 +1,20 @@
 package io.ticktag.restinterface.hello.controllers
 
+import io.ticktag.TicktagRestInterface
 import io.ticktag.restinterface.hello.schema.HelloResultJson
-import io.ticktag.service.hello.dto.HelloResult
+import io.ticktag.service.hello.dto.HelloParams
+import io.ticktag.service.hello.services.HelloService
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 
-interface HelloController {
-    fun hello(firstname: String, lastname: String): HelloResultJson
+@TicktagRestInterface
+@RequestMapping("/hello")
+open class HelloController(
+        private val hello: HelloService
+) {
+    @RequestMapping(method = arrayOf(RequestMethod.GET))
+    fun hello(@RequestParam firstname: String, @RequestParam lastname: String): HelloResultJson {
+        return HelloResultJson(hello.hello(HelloParams(firstname, lastname)))
+    }
 }
