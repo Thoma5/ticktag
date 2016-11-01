2af2b2f97c577ab00a87ff31e4a283832d845bd8
Simon Strassl <stuff@sigmoid.at>
2016-11-01 13:36:43 +0100

    Create maven project

---
+++ backend/src/main/java/io/ticktag/TicktagApplication.java
@@ -0,0 +1,6 @@
+package io.ticktag;
+
+public class TicktagApplication {
+    public static void main(String[] args) {
+    }
+}
---
+++ backend/src/test/java/io/ticktag/DummyTest.java
@@ -0,0 +1,12 @@
+package io.ticktag;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class DummyTest {
+    @Test
+    public void test() {
+    }
+}
