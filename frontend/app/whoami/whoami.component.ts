366c2da8af086322bed5a7540047bafb08137ef1
Simon Strassl <stuff@sigmoid.at>
2016-11-03 16:55:04 +0100

    Add simple whoami page

--- frontend/app/whoami/whoami.component.ts
+++ frontend/app/whoami/whoami.component.ts
@@ -1,20 +1,28 @@
 import {Component, OnInit} from "@angular/core";
 import {AuthApi} from "../api/api/AuthApi";
 import {WhoamiResult} from "../api/model/WhoamiResult";
+import {Headers} from "@angular/http";
 
 @Component({
     moduleId: module.id,
     selector: 'whoami',
     templateUrl: 'whoami.component.html',
 })
-export class WhoamiComponent implements OnInit{
+export class WhoamiComponent implements OnInit {
+    private token:string;
     private me:WhoamiResult;
 
     constructor(private authApi:AuthApi) {
     }
 
     ngOnInit():void {
-        this.authApi.whoamiUsingGET()
+    }
+
+    getWhoami():void {
+        let headers = new Headers();
+        headers.append('X-Authorization', this.token);
+
+        this.authApi.whoamiUsingGET({'headers': headers})
             .subscribe(res => this.me = res, err => alert(err));
     }
 }
