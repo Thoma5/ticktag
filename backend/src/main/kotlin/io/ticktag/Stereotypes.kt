221a05d4af2cd363970575461e0cd8657a08de6a
Simon Strassl <stuff@sigmoid.at>
2016-11-03 15:36:10 +0100

    Allow requests from any origin

--- backend/src/main/kotlin/io/ticktag/Stereotypes.kt
+++ backend/src/main/kotlin/io/ticktag/Stereotypes.kt
@@ -5,6 +5,7 @@ import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -13,6 +14,7 @@ annotation class TicktagRepository
 
 @RestController
 @RequestMapping(produces = arrayOf("application/json"), consumes = arrayOf("application/json"))
+@CrossOrigin(origins = arrayOf("*"))
 annotation class TicktagRestInterface
 
 @Service
