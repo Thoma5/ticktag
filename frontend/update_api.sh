5040529bfc6d4ccbc787c4164cdeee11299a3302
Simon Strassl <stuff@sigmoid.at>
2016-11-03 15:05:39 +0100

    Remove old code when generating api bindings

--- frontend/update_api.sh
+++ frontend/update_api.sh
@@ -3,4 +3,6 @@
 # This script pulls the api spec from the local server and generates the Angular2 bindings to app/api
 # Make sure the server is running on localhost:8080
 
-java -jar swagger-codegen-cli.jar generate -i "http://localhost:8080/v2/api-docs" -l "typescript-angular2" -o app/api
+java -jar swagger-codegen-cli.jar generate -i "http://localhost:8080/v2/api-docs" -l "typescript-angular2" -o ./api_new
+rm -r app/api
+mv ./api_new app/api
