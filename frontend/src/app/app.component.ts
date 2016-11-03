6f8ddb6d3e16a745ead19254b3cd71894ca47e7f
Simon Strassl <stuff@sigmoid.at>
2016-11-04 00:45:58 +0100

    Add super awesome logout button

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,30 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import '../style/app.scss';
+import {AuthService} from './service/auth/auth.service';
 
 @Component({
   selector: 'tt-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   private title;
+  private loggedIn;
 
-  constructor() {
+  constructor(private authService: AuthService) {
     this.title = 'TickTag';
+  }
+
+  ngOnInit(): void {
+    this.loggedIn = this.authService.getToken() != null;
+    this.authService.observeToken()
+      .subscribe(token => {
+        console.log(token);
+        this.loggedIn = token != null;
+      });
+  }
+
+  logout(): void {
+    this.authService.setToken(null);
   }
 }
--- frontend/src/app/service/auth/auth.service.ts
+++ frontend/src/app/service/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {LocalStorageService} from 'ng2-webstorage';
+import {Observable} from 'rxjs/Rx';
 
 const KEY_TOKEN = 'token';
 
@@ -19,5 +20,9 @@ export class AuthService {
 
   setToken(token: string|null): void {
     this.localStorageService.store(KEY_TOKEN, token);
+  }
+
+  observeToken(): Observable<string|null> {
+    return this.localStorageService.observe(KEY_TOKEN).asObservable();
   }
 }
