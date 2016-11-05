4c082159b712a71c9273790ae17e8c1065d84755
Simon Strassl <stuff@sigmoid.at>
2016-11-05 11:08:31 +0100

    Make update_api fail on error

--- frontend/update_api.sh
+++ frontend/update_api.sh
@@ -6,6 +6,4 @@
 # Also, swagger-codegen 2.2.0 does not generate correct bindings (no content-type headers are generated) which is why the version here is built from the (not yet released) 2.3.0 sources
 # See: https://github.com/swagger-api/swagger-codegen
 
-java -jar swagger-codegen-cli.jar generate -i "http://localhost:8080/v2/api-docs" -l "typescript-angular2" -o ./api_new
-rm -r src/app/api
-mv ./api_new src/app/api
+java -jar swagger-codegen-cli.jar generate -i "http://localhost:8080/v2/api-docs" -l "typescript-angular2" -o ./api_new && (rm -r src/app/api || true) && mv ./api_new src/app/api
