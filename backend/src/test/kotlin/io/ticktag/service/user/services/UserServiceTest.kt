ed03174682e90320ee4c553c96748bc08c7c2888
Michael Heinzl <m.heinzl@xforge.at>
2016-11-13 15:46:00 +0100

    update tests for principal rework

--- backend/src/test/kotlin/io/ticktag/service/user/services/UserServiceTest.kt
+++ backend/src/test/kotlin/io/ticktag/service/user/services/UserServiceTest.kt
@@ -1,6 +1,8 @@
 package io.ticktag.service.user.services;
 
 import io.ticktag.BaseTest
+import io.ticktag.persistence.user.entity.Role
+import io.ticktag.persistence.user.entity.User
 import io.ticktag.service.Principal
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertNull
@@ -19,7 +21,8 @@ class UserServiceTest : BaseTest() {
 
     @Test
     fun test_checkPassword_negative(){
-        val p = Principal(UUID.fromString("00000000-0001-0000-0000-000000000001"), setOf("USER","OBSERVER","ADMIN"))
+        val user = User.createWithId(UUID.fromString("00000000-0001-0000-0000-000000000001"), "", "", "", Role.ADMIN, UUID.fromString("00000000-0001-0000-0000-000000000001"))
+        val p = Principal(UUID.fromString("00000000-0001-0000-0000-000000000001"), setOf("USER","OBSERVER","ADMIN"), user)
         SecurityContextHolder.getContext().authentication = PreAuthenticatedAuthenticationToken(p,null,p.authorities.map { SimpleGrantedAuthority(it) })
         val validLogin = this.userService.checkPassword("a@a.a", "fail")
         assertNull(validLogin)
@@ -27,7 +30,8 @@ class UserServiceTest : BaseTest() {
 
     @Test
     fun test_checkPassword_positive(){
-        val p = Principal(UUID.fromString("00000000-0001-0000-0000-000000000001"), setOf("USER","OBSERVER","ADMIN"))
+        val user = User.createWithId(UUID.fromString("00000000-0001-0000-0000-000000000001"), "", "", "", Role.ADMIN, UUID.fromString("00000000-0001-0000-0000-000000000001"))
+        val p = Principal(UUID.fromString("00000000-0001-0000-0000-000000000001"), setOf("USER","OBSERVER","ADMIN"), user)
         SecurityContextHolder.getContext().authentication = PreAuthenticatedAuthenticationToken(p,null,p.authorities.map { SimpleGrantedAuthority(it) })
         val validLogin = this.userService.checkPassword("a@a.a", "aaaa")
         assertNotNull(validLogin)
