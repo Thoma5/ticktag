b6f25569f891408fdf44e9306bcfd8c02629198c
Michael Heinzl <m.heinzl@xforge.at>
2016-11-15 19:22:46 +0100

    Fix max size of project name and description for create and update

--- backend/src/main/kotlin/io/ticktag/service/project/dto/CreateProject.kt
+++ backend/src/main/kotlin/io/ticktag/service/project/dto/CreateProject.kt
@@ -3,7 +3,7 @@ package io.ticktag.service.project.dto
 import javax.validation.constraints.Size
 
 data class CreateProject(
-        @field:Size(min = 3, max = 255) val name: String,
-        @field:Size(min = 3, max = 30) val description: String,
+        @field:Size(min = 3, max = 30) val name: String,
+        @field:Size(min = 3, max = 255) val description: String,
         @field:Size(max = 204800) val icon: ByteArray?
 )
