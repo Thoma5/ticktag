279d194e789d66b693b61f022234cefd5b9ece46
Draskovits Stefan <s.draskovits@xforge.at>
2016-11-16 21:41:14 +0100

    extend TicketResultJson

--- backend/src/main/kotlin/io/ticktag/restinterface/ticket/schema/TicketResultJson.kt
+++ backend/src/main/kotlin/io/ticktag/restinterface/ticket/schema/TicketResultJson.kt
@@ -1,10 +1,26 @@
 package io.ticktag.restinterface.ticket.schema
 
+import io.ticktag.persistence.project.entity.Project
+import io.ticktag.persistence.ticket.entity.Ticket
+import io.ticktag.persistence.user.entity.User
 import io.ticktag.service.ticket.dto.TicketResult
+import java.time.Duration
+import java.time.Instant
 import java.util.*
 
 class TicketResultJson (
-    val id: UUID
+        val id: UUID,
+        val number:Int,
+        val createTime:Instant,
+        val title: String,
+        val open:Boolean,
+        val storyPoints:Int?,
+        val initialEstimatedTime:Duration?,
+        val currentEstimatedTime: Duration?,
+        val dueDate: Instant?
+
 ){
-    constructor(t: TicketResult) : this(id = t.id)
+    constructor(t: TicketResult) : this(id = t.id,number = t.number,createTime =  t.createTime, title = t.title,
+                open = t.open,storyPoints = t.storyPoints,initialEstimatedTime = t.initialEstimatedTime,currentEstimatedTime = t.currentEstimatedTime,
+            dueDate = t.dueDate)
 }
--- backend/src/main/kotlin/io/ticktag/service/ticket/dto/TicketResult.kt
+++ backend/src/main/kotlin/io/ticktag/service/ticket/dto/TicketResult.kt
@@ -1,11 +1,27 @@
 package io.ticktag.service.ticket.dto
 
+import io.ticktag.persistence.project.entity.Project
+import io.ticktag.persistence.ticket.entity.Comment
 import io.ticktag.persistence.ticket.entity.Ticket
+import io.ticktag.persistence.user.entity.User
+import java.time.Duration
+import java.time.Instant
 import java.util.*
 
 
 data class TicketResult(
-        val id: UUID
+        val id: UUID,
+        val number:Int,
+        val createTime:Instant,
+        val title: String,
+        val open:Boolean,
+        val storyPoints:Int?,
+        val initialEstimatedTime:Duration?,
+        val currentEstimatedTime: Duration?,
+        val dueDate: Instant?,
+        val description: String
 ) {
-    constructor(t: Ticket) : this(id = t.id)
+    constructor(t: Ticket) : this(id = t.id,number = t.number,createTime = t.createTime,title = t.title,
+                open = t.open,storyPoints = t.storyPoints, initialEstimatedTime = t.initialEstimatedTime,currentEstimatedTime = t.currentEstimatedTime,
+            dueDate = t.dueDate,description = t.descriptionComment.text)
 }
