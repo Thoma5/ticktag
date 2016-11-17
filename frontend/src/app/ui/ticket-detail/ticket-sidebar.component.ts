46cd2e77dcc251201bb9e734f8f4e36c15b16dd9
Simon Strassl <stuff@sigmoid.at>
2016-11-17 19:33:43 +0100

    Remove unused imports from sidebar

--- frontend/src/app/ui/ticket-detail/ticket-sidebar.component.ts
+++ frontend/src/app/ui/ticket-detail/ticket-sidebar.component.ts
@@ -1,10 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Router, ActivatedRoute, Params } from '@angular/router';
-// import { Response, ResponseContentType } from '@angular/http';
-import { ApiCallService } from '../../service';
+import { Component, Input } from '@angular/core';
 import { TicketResultJson } from './ticket-detail.component';
-import { Observable } from 'rxjs';
-import {Duration, Instant} from 'js-joda';
 
 @Component({
   selector: 'tt-ticket-sidebar',
