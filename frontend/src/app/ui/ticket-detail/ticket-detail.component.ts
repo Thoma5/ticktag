41a5c024e88f057f6f3ad28796a07b54b6d37685
Simon Strassl <stuff@sigmoid.at>
2016-11-17 22:28:06 +0100

    Add more description text

--- frontend/src/app/ui/ticket-detail/ticket-detail.component.ts
+++ frontend/src/app/ui/ticket-detail/ticket-detail.component.ts
@@ -23,7 +23,13 @@ class MockTicketApi {
       number: 123,
       createTime: moment.utc(0),
       title: 'Implement ticket details UI',
-      description: 'It is severly lacking at the moment',
+      /* tslint:disable */
+      description:
+      `Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.
+      Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.
+      Capitalise on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.
+      `,
+      /* tslint:enable */
       open: false,
       storyPoints: 8,
       initialEstimatedTime: moment.duration(2, 'hours'),
