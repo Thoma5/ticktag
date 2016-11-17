8f4b8b3e11bcd15cbc4e6665358c40851df6415e
Michael Heinzl <m.heinzl@xforge.at>
2016-11-17 18:12:45 +0100

    Add ticketTag rest interface

---
+++ backend/src/main/kotlin/io/ticktag/restinterface/tickettag/schema/CreateTicketTagRequestJson.kt
@@ -0,0 +1,10 @@
+package io.ticktag.restinterface.tickettag.schema
+
+import java.util.*
+
+data class CreateTicketTagRequestJson(
+        val name: String,
+        val color: String,
+        val order: Int,
+        val ticketTagGroupId: UUID
+)
