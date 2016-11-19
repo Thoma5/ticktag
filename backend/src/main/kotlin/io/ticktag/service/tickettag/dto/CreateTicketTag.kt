ca3feaf33860a74804413c24d2906165d9201b83
Michael Heinzl <m.heinzl@xforge.at>
2016-11-19 19:07:32 +0100

    Add regex to TicketTag

--- backend/src/main/kotlin/io/ticktag/service/tickettag/dto/CreateTicketTag.kt
+++ backend/src/main/kotlin/io/ticktag/service/tickettag/dto/CreateTicketTag.kt
@@ -1,9 +1,10 @@
 package io.ticktag.service.tickettag.dto
 
+import javax.validation.constraints.Pattern
 import javax.validation.constraints.Size
 
 data class CreateTicketTag(
     @field:Size(min = 3, max = 30) val name: String,
-    @field:Size(min = 6, max = 6) val color: String,
+    @field:Pattern(regexp = "^(?:[0-9a-fA-F]{3}){1,2}$") val color: String, //RGB HEX Color
     val order: Int
 )
