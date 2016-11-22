bb18a0c4e54b83dfaf4cd7134da0a08375579575
Christoph Kiss <e1326346@student.tuwien.ac.at>
2016-11-22 13:21:12 +0100

    Reworked assignment tag Rest API

--- backend/src/main/kotlin/io/ticktag/persistence/ticket/AssignmentTagRepository.kt
+++ backend/src/main/kotlin/io/ticktag/persistence/ticket/AssignmentTagRepository.kt
@@ -9,7 +9,8 @@ import java.util.*
 interface AssignmentTagRepository : TicktagCrudRepository<AssignmentTag, UUID> {
     fun findByProjectId(projectId: UUID): List<AssignmentTag>
 
-    fun findByProjectIdAndNameLikeIgnoreCase(projectId: UUID, name: String): List<AssignmentTag>
+    //No server side search function
+    //fun findByProjectIdAndNameLikeIgnoreCase(projectId: UUID, name: String): List<AssignmentTag>
 
     fun findByNormalizedNameAndProjectId(normalizedName: String, projectId: UUID): AssignmentTag?
  }
