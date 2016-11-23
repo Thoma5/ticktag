3bd9c60af75faa945fc9e756424f6f5fb8630e61
Michael Heinzl <m.heinzl@xforge.at>
2016-11-23 13:37:47 +0100

    Extend event service to sub classes

--- backend/src/main/kotlin/io/ticktag/service/ticketevent/services/impl/TicketEventServiceImpl.kt
+++ backend/src/main/kotlin/io/ticktag/service/ticketevent/services/impl/TicketEventServiceImpl.kt
@@ -2,12 +2,9 @@ package io.ticktag.service.ticketevent.services.impl
 
 import io.ticktag.TicktagService
 import io.ticktag.persistence.ticket.TicketEventRepository
-import io.ticktag.persistence.ticket.entity.TicketEventCommentTextChanged
-import io.ticktag.persistence.ticket.entity.TicketEventTitleChanged
+import io.ticktag.persistence.ticket.entity.*
 import io.ticktag.service.AuthExpr
-import io.ticktag.service.ticketevent.dto.TicketEventCommentTextChangedResult
-import io.ticktag.service.ticketevent.dto.TicketEventResult
-import io.ticktag.service.ticketevent.dto.TicketEventTitleChangedResult
+import io.ticktag.service.ticketevent.dto.*
 import io.ticktag.service.ticketevent.services.TicketEventService
 import org.springframework.security.access.method.P
 import org.springframework.security.access.prepost.PreAuthorize
@@ -25,6 +22,20 @@ open class TicketEventServiceImpl @Inject constructor(
             when (e) {
                 is TicketEventTitleChanged -> TicketEventTitleChangedResult(e)
                 is TicketEventCommentTextChanged -> TicketEventCommentTextChangedResult(e)
+                is TicketEventCurrentEstimatedTimeChanged -> TicketEventCurrentEstimatedTimeChangedResult(e)
+                is TicketEventDueDateChanged -> TicketEventDueDateChangedResult(e)
+                is TicketEventInitialEstimatedTimeChanged -> TicketEventInitialEstimatedTimeChangedResult(e)
+                is TicketEventLoggedTimeAdded -> TicketEventLoggedTimeAddedResult(e)
+                is TicketEventLoggedTimeRemoved -> TicketEventLoggedTimeRemovedResult(e)
+                is TicketEventMentionAdded -> TicketEventMentionAddedResult(e)
+                is TicketEventMentionRemoved -> TicketEventMentionRemovedResult(e)
+                is TicketEventParentChanged -> TicketEventParentChangedResult(e)
+                is TicketEventStateChanged -> TicketEventStateChangedResult(e)
+                is TicketEventStoryPointsChanged -> TicketEventStoryPointsChangedResult(e)
+                is TicketEventTagAdded -> TicketEventTagAddedResult(e)
+                is TicketEventTagRemoved -> TicketEventTagRemovedResult(e)
+                is TicketEventUserAdded -> TicketEventUserAddedResult(e)
+                is TicketEventUserRemoved -> TicketEventUserRemovedResult(e)
                 else -> TicketEventResult(e)
             }
         }
