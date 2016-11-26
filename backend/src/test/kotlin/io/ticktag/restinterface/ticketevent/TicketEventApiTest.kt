48fd56774205289423dd825773e666d4badf7407
Christoph Kiss <e1326346@student.tuwien.ac.at>
2016-11-26 18:10:36 +0100

    Added Tests

--- backend/src/test/kotlin/io/ticktag/restinterface/ticketevent/TicketEventApiTest.kt
+++ backend/src/test/kotlin/io/ticktag/restinterface/ticketevent/TicketEventApiTest.kt
@@ -1,10 +1,19 @@
+package io.ticktag.restinterface.ticketevent
+
+
+import io.ticktag.ASSTAG_PROJECT_AOU_OAU_IDS
+import io.ticktag.TICKET_PROJECT_AOU_OAU_0_ID
 import io.ticktag.USER_ID
+import io.ticktag.USER_USER_ID
 import io.ticktag.restinterface.ApiBaseTest
+import io.ticktag.restinterface.assignmenttag.controllers.AssignmentTagController
+import io.ticktag.restinterface.assignmenttag.schema.CreateAssignmentTagRequestJson
 import io.ticktag.restinterface.comment.controllers.CommentController
 import io.ticktag.restinterface.comment.schema.UpdateCommentRequestJson
 import io.ticktag.restinterface.loggedTime.controller.LoggedTimeController
 import io.ticktag.restinterface.ticket.controllers.TicketController
 import io.ticktag.restinterface.ticket.schema.UpdateTicketRequestJson
+import io.ticktag.restinterface.ticketassignment.controllers.TicketAssignmentController
 import io.ticktag.restinterface.ticketevent.controllers.TicketEventController
 import io.ticktag.service.Principal
 import io.ticktag.service.comment.dto.CreateLoggedTimeJson
@@ -20,29 +29,23 @@ class TicketEventApiTest : ApiBaseTest() {
     @Inject lateinit var ticketController: TicketController
     @Inject lateinit var commentController: CommentController
     @Inject lateinit var loggedTimeController: LoggedTimeController
+    @Inject lateinit var ticketAssignmentController: TicketAssignmentController
+    @Inject lateinit var assignmentTagController: AssignmentTagController
 
     val ticketId = UUID.fromString("00000000-0003-0000-0000-000000000006")
+    val parentTicketId = UUID.fromString("00000000-0003-0000-0000-000000000001")
     val commendId = UUID.fromString("00000000-0004-0000-0000-000000000008")
+    val assignmentTagId = UUID.fromString("00000000-0006-0000-0000-000000000001")
 
-
-    fun updateTicket(principal: Principal) {
-        ticketController.updateTicket(UpdateTicketRequestJson("New Title", false, 20, Duration.ofMinutes(50), Duration.ofMinutes(100), Instant.parse("2016-11-16T16:00:00Z"), "Added Models to Layout", null, null, null, null), ticketId, principal)
+    @Test
+    fun test_ticketDescriptionChangedShouldAddEvent() {
+        withUser(USER_ID) { principal ->
+            val sizeBefore = ticketEventController.listTicketEvents(ticketId).size
+            val ticket = ticketController.getTicket(ticketId)
+            ticketController.updateTicket(UpdateTicketRequestJson(ticket.title, ticket.open, ticket.storyPoints, ticket.initialEstimatedTime, ticket.currentEstimatedTime, ticket.dueDate, "New Description", null, null, null, null), ticketId, principal)
+            Assert.assertEquals(ticketEventController.listTicketEvents(ticketId).size, sizeBefore + 1)
+        }
     }
-
-    fun keepTicketSame(principal: Principal) {
-        val ticket = ticketController.getTicket(ticketId)
-        ticketController.updateTicket(UpdateTicketRequestJson(ticket.title, true, ticket.storyPoints, ticket.initialEstimatedTime, ticket.currentEstimatedTime, ticket.dueDate, ticket.description, null, null, null, null), ticketId, principal)
-    }
-
-    fun updateComment(principal: Principal) {
-        //  commentController.updateComment(UpdateCommentRequestJson("Changed Comment"), , commendId, principal)
-    }
-
-    fun keepCommentSame(principal: Principal) {
-        val comment = commentController.getComment(commendId)
-        commentController.updateComment(UpdateCommentRequestJson(comment.text, comment.mentionedTicketId, null), commendId, principal)
-    }
-
 
     @Test
     fun test_ticketEventCommentChangedShouldAddEvent() {
@@ -65,14 +68,43 @@ class TicketEventApiTest : ApiBaseTest() {
     }
 
     @Test
-    fun test_ticketLoggedTimeAddedShouldAddEvent() {
-        val categoryId = UUID.fromString("00000000-0007-0000-0000-000000000001")
-
+    fun test_ticketDueDateChangedShouldAddEvent() {
         withUser(USER_ID) { principal ->
             val sizeBefore = ticketEventController.listTicketEvents(ticketId).size
-            loggedTimeController.createLoggedTime(CreateLoggedTimeJson(Duration.ofHours(1), commendId, categoryId))
-
             val ticket = ticketController.getTicket(ticketId)
+            ticketController.updateTicket(UpdateTicketRequestJson(ticket.title, ticket.open, ticket.storyPoints, ticket.initialEstimatedTime, ticket.currentEstimatedTime, Instant.parse("2017-01-01T16:00:00Z"), ticket.description, null, null, null, null), ticketId, principal)
+            Assert.assertEquals(ticketEventController.listTicketEvents(ticketId).size, sizeBefore + 1)
+        }
+    }
+
+
+    /** Todo- Implement this feature
+    @Test
+    fun test_ticketLoggedTimeAddedShouldAddEvent() {
+    val categoryId = UUID.fromString("00000000-0007-0000-0000-000000000001")
+
+    withUser(USER_ID) { principal ->
+    val sizeBefore = ticketEventController.listTicketEvents(ticketId).size
+    loggedTimeController.createLoggedTime(CreateLoggedTimeJson(Duration.ofHours(1), commendId, categoryId))
+
+    val ticket = ticketController.getTicket(ticketId)
+    Assert.assertEquals(ticketEventController.listTicketEvents(ticketId).size, sizeBefore + 1)
+    }
+    }
+     **/
+
+    //TODO logged time removed
+
+    //TODO mention added
+
+    //TODO mention removed
+
+
+    fun test_parentChangedShouldAddEvent() {
+        withUser(USER_ID) { principal ->
+            val sizeBefore = ticketEventController.listTicketEvents(ticketId).size
+            val ticket = ticketController.getTicket(ticketId)
+            ticketController.updateTicket(UpdateTicketRequestJson(ticket.title, ticket.open, ticket.storyPoints, ticket.initialEstimatedTime, ticket.currentEstimatedTime, Instant.parse("2017-01-01T16:00:00Z"), ticket.description, null, null, null, parentTicketId), ticketId, principal)
             Assert.assertEquals(ticketEventController.listTicketEvents(ticketId).size, sizeBefore + 1)
         }
     }
@@ -88,14 +120,48 @@ class TicketEventApiTest : ApiBaseTest() {
     }
 
     @Test
-    fun test_ticketDescriptionChangedShouldAddEvent() {
+    fun test_ticketStateChangedShouldAddEvent() {
         withUser(USER_ID) { principal ->
             val sizeBefore = ticketEventController.listTicketEvents(ticketId).size
             val ticket = ticketController.getTicket(ticketId)
-            ticketController.updateTicket(UpdateTicketRequestJson(ticket.title, ticket.open, ticket.storyPoints, ticket.initialEstimatedTime, ticket.currentEstimatedTime, ticket.dueDate, "New Description", null, null, null, null), ticketId, principal)
+            ticketController.updateTicket(UpdateTicketRequestJson(ticket.title, !ticket.open, ticket.storyPoints, ticket.initialEstimatedTime, ticket.currentEstimatedTime, ticket.dueDate, ticket.description, null, null, null, null), ticketId, principal)
             Assert.assertEquals(ticketEventController.listTicketEvents(ticketId).size, sizeBefore + 1)
         }
     }
+
+    @Test
+    fun test_ticketStoryPointsChangedShouldAddEvent() {
+        withUser(USER_ID) { principal ->
+            val sizeBefore = ticketEventController.listTicketEvents(ticketId).size
+            val ticket = ticketController.getTicket(ticketId)
+            ticketController.updateTicket(UpdateTicketRequestJson(ticket.title, ticket.open, 100, ticket.initialEstimatedTime, ticket.currentEstimatedTime, ticket.dueDate, ticket.description, null, null, null, null), ticketId, principal)
+            Assert.assertEquals(ticketEventController.listTicketEvents(ticketId).size, sizeBefore + 1)
+        }
+    }
+
+    //TODO Tags added
+
+    //TODO Tags removed
+
+    /** Todo Check
+    @Test
+    fun test_ticketUserAddedChangedShouldAddEvent() {
+        withUser(USER_ID) { principal ->
+            val sizeBefore = ticketEventController.listTicketEvents(ticketId).size
+             ticketAssignmentController.createTicketAssignment(ticketId, assignmentTagId, principal.id, principal)
+            Assert.assertEquals(ticketEventController.listTicketEvents(ticketId).size, sizeBefore + 1)
+        }
+    }
+
+    @Test
+    fun test_ticketUserRemovedChangedShouldAddEvent() {
+        withUser(USER_ID) { principal ->
+            ticketAssignmentController.createTicketAssignment(ticketId, assignmentTagId, principal.id, principal)
+            val sizeBefore = ticketEventController.listTicketEvents(ticketId).size
+            ticketAssignmentController.deleteTicketAssignment(ticketId, assignmentTagId, principal.id, principal)
+            Assert.assertEquals(ticketEventController.listTicketEvents(ticketId).size, sizeBefore + 1)
+        }
+    }**/
 
     /** Negative Test for all Events **/
 
