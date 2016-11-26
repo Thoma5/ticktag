f72d678005a4feae2e8ca9b2e73201c8a4a852d4
Thomas Moerbauer <tmoer93@gmail.com>
2016-11-26 20:40:29 +0100

    Added not working Pagination to the Project Frontend

--- frontend/src/app/ui/projects/projects.component.ts
+++ frontend/src/app/ui/projects/projects.component.ts
@@ -7,7 +7,7 @@ import { ProjectApi, PageProjectResultJson } from '../../api';
   templateUrl: './projects.component.html',
 })
 export class ProjectsComponent implements OnInit {
-  projects: PageProjectResultJson;
+  private projects: PageProjectResultJson;
   columns = [
     { name: 'ID' },
     { name: 'Name' },
@@ -15,6 +15,9 @@ export class ProjectsComponent implements OnInit {
     { name: 'CreationDate' },
     { name: 'Icon' }
   ];
+  private loading = true;
+  private offset = 0;
+  private limit = 10;
 
   // TODO make readonly once Intellij supports readonly properties in ctr
   constructor(private projectApi: ProjectApi,
@@ -23,12 +26,20 @@ export class ProjectsComponent implements OnInit {
 
   ngOnInit(): void {
     this.projects = [];
-    this.getProjects();
+    this.getProjects(this.offset, this.limit, 'NAME', true, undefined);
   }
 
   getProjects(page?: number, size?: number, order?: string, asc?: boolean, name?: string): void {
     this.apiCallService
       .callNoError<PageProjectResultJson>(h => this.projectApi.listProjectsUsingGETWithHttpInfo(page, size, order, asc, name, true, h))
-      .subscribe(projects => { this.projects = projects; });
+      .subscribe(projects => {
+        this.projects = projects;
+        this.loading = false;
+      });
   }
+  onPage() {
+    this.loading = true;
+    this.getProjects(++this.offset, this.limit, 'NAME', true, undefined);
+  }
+
 }
