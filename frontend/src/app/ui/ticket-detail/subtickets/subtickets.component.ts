d18154d369e89cade66fc3d9e230ac9804f0e8ac
Simon Strassl <stuff@sigmoid.at>
2016-12-04 20:01:39 +0100

    Ensure subtickets are sorted by number

--- frontend/src/app/ui/ticket-detail/subtickets/subtickets.component.ts
+++ frontend/src/app/ui/ticket-detail/subtickets/subtickets.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 import { TicketDetail, TicketDetailRelated } from '../ticket-detail';
 import { TicketRestoreEvent } from '../subticket/subticket.component';
 import * as imm from 'immutable';
@@ -6,11 +6,19 @@ import * as imm from 'immutable';
 @Component({
   selector: 'tt-subtickets',
   templateUrl: './subtickets.component.html',
-  styleUrls: ['./subtickets.component.scss']
+  styleUrls: ['./subtickets.component.scss'],
 })
-export class SubticketsComponent {
+export class SubticketsComponent implements OnChanges {
   @Input() parentTicket: TicketDetail;
   @Input() tickets: imm.List<TicketDetailRelated>;
 
   @Output() readonly ticketRestore = new EventEmitter<TicketRestoreEvent>();
+
+  private sortedTickets: imm.List<TicketDetailRelated>;
+
+  ngOnChanges(changes: SimpleChanges) {
+    if ('tickets' in changes) {
+      this.sortedTickets = this.tickets.sort((a, b) => a.number - b.number).toList();
+    }
+  }
 }
