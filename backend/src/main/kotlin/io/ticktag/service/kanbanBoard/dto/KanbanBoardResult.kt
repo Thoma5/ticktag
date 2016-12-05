99dd07595c7a2db57d3e684fb9755909cdbe7427
Draskovits Stefan <s.draskovits@xforge.at>
2016-12-05 09:33:53 +0100

    remove unnecessary code

--- backend/src/main/kotlin/io/ticktag/service/kanbanBoard/dto/KanbanBoardResult.kt
+++ backend/src/main/kotlin/io/ticktag/service/kanbanBoard/dto/KanbanBoardResult.kt
@@ -7,8 +7,7 @@ import java.util.*
 data class KanbanBoardResult(
         val id: UUID,
         val name: String,
-        val project_id: UUID,
-        val columnIds: List<UUID>
+        val project_id: UUID
 ) {
-    constructor(g: TicketTagGroup) : this(id = g.id, name = g.name, project_id = g.project.id, columnIds = g.ticketTags.map { it.id })
+    constructor(g: TicketTagGroup) : this(id = g.id, name = g.name, project_id = g.project.id)
 }
