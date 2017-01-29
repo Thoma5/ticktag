fef528bdb9f4b7dcb51cca5a04dcc5614176e0e6
Simon Strassl <stuff@sigmoid.at>
2017-01-29 17:41:47 +0100

    Redirect home component to projects

--- frontend/src/app/ui/home/home.component.ts
+++ frontend/src/app/ui/home/home.component.ts
@@ -1,9 +1,17 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'tt-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.router.navigate(['/projects']);
+  }
 }
